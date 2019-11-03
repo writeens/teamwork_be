81db75bfc21288b4c5111ce52e31e856446fae64
writeens <44687555+writeens@users.noreply.github.com>
2019-11-03 18:18:01 +0100

    set up DB credentials for postgres

---
+++ database/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+// DB connection string
+const connectString = 'postgres://teamwork:teamwork@localhost/teamwork';
+
+
+// Create a pool
+const pool = new Pool({
+  connectionString: connectString,
+});
+
+module.exports = { pool };
